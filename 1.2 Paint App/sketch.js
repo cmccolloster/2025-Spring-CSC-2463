ef48b5134c56b19060a964e9d091bfd8201d7e2d
Chris M <cmccol6@lsu.edu>
2025-01-28 13:28:48 -0600

    Adding folder and contents

---
+++ 1.2 Paint App/sketch.js
@@ -0,0 +1,14 @@
+// Name: Christopher McColloster
+// Date: 1/30/25
+// Class: CSC 2463
+// Teacher: Andrew Webb
+// Assignment: 1.2 Paint App
+
+function setup() {
+  createCanvas(280, 700);
+  colorMode(HSB);
+}
+
+function draw() {
+  
+}
